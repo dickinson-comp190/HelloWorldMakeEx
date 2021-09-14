1f1cc01fb9ebf715bc8e6ec4bfdd2401640da3b8
Grant Braught <braughtg@gmail.com>
2021-09-14 15:39:35 -0400

    Added gitignore

--- main.c
+++ main.c
@@ -4,9 +4,19 @@
 #include <libintl.h>
 #include <locale.h>
 
+// Define the common macro substitution for gettext.
+#define _(STRING) gettext(STRING)
+
 int
 main(int argc, char* argv[])
 {
-    printf("Hello world\n");
-    return 0;
+    // Setup the i18n parameters.
+    setlocale (LC_ALL, "");
+    bindtextdomain ("hello", getenv("PWD"));
+    textdomain ("hello");
+
+    // Translate the string and print it.
+    printf(_("Hello world\n"));
+
+    return EXIT_SUCCESS;
 }
